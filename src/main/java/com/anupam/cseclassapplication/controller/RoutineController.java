f2b56e0382d365cda7be3614d32af31cdabda324
NoobCoderAntu <anupamdey2022cse@gmail.com>
2023-10-03 03:09:38 +0600

    Edit button working Perfectlyon web app

--- src/main/java/com/anupam/cseclassapplication/controller/RoutineController.java
+++ src/main/java/com/anupam/cseclassapplication/controller/RoutineController.java
@@ -2,19 +2,27 @@ package com.anupam.cseclassapplication.controller;
 
 import java.time.Instant;
 import java.time.ZoneId;
-
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.anupam.cseclassapplication.entity.DatabaseEntity;
 import com.anupam.cseclassapplication.repo.EntityRepo;
+
+import jakarta.validation.Valid;
 
 
 @Controller
 public class RoutineController {
+    
     @Autowired
     EntityRepo entityRepo;
+    
     @GetMapping("/")
     public ModelAndView index() {
         ModelAndView modelAndView = new ModelAndView("index");
@@ -22,5 +30,22 @@ public class RoutineController {
         modelAndView.addObject("RoutineItems", entityRepo.findAll()); 
         modelAndView.addObject("Today", Instant.now().atZone(ZoneId.systemDefault()).toLocalDate().getDayOfWeek());
         return modelAndView;
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showUpdateForm (@PathVariable("id") long id, Model model) {
+         
+        DatabaseEntity databaseEntity = entityRepo.findById(id).orElseThrow(() -> new IllegalArgumentException("RoutineItem id : "+id + " not found"));
+
+        model.addAttribute("Routine", databaseEntity);
+        return "edit-routine-item";
+    }
+
+    @PostMapping("/Routine/{id}")
+    public String updateRoutineItem(@PathVariable("id") long id, DatabaseEntity databaseEntity, Model model){
+        
+        entityRepo.save(databaseEntity);
+        return "redirect:/";
+
     }
 }
